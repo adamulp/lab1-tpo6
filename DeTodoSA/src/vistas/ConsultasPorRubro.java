6eb6f7a9c47a6dfa2aa316adf30950d12b5740d7
adamulp <adamgoestoargentina@gmail.com>
2024-04-13 11:57:28 -0300

    Actualizar nombres y compareTo()

--- DeTodoSA/src/vistas/ConsultasPorRubro.java
+++ DeTodoSA/src/vistas/ConsultasPorRubro.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JInternalFrame.java to edit this template
  */
-package Vistas;
+package vistas;
 
-import Producto.Rubro.Rubro;
+import entidades.Rubro;
 
 /**
  *
